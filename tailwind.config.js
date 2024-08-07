b521340a9bcb468c26652944bfffd3df37788298
HusseinBaraja <hussainbaraja34@gmail.com>
2024-08-07 14:18:21 +0300

    tailwind.config.js fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
       },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+    },
   },
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
